66ba2e45b29bddd86afa8439bcdcca5cd7b2662e
Kristoffer Hartvig <hartvigkristoffer@gmail.com>
2025-11-17 11:24:05 +0100

    added comments

--- src/wind.c
+++ src/wind.c
@@ -2,7 +2,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define WIND_SPEED_MIN 0.5
 #define WIND_SPEED_MAX 15.0
@@ -13,15 +12,14 @@ void start_wind(struct Wind* wind) {
 }
 
 void update_wind(struct Wind* wind) {
-    //srand(time(NULL));
-
+    //Tilfældig speed mellem max og min.
     double randomSpeed = ((double) rand() / RAND_MAX) * (WIND_SPEED_MAX - WIND_SPEED_MIN) + WIND_SPEED_MIN;
 
     wind->speed =  randomSpeed;
-    wind->direction = rand()%4; //Tilfædig direktion mellem (0-3)
+    wind->direction = rand()%4; //Tilfældig direktion mellem (0-3)
 }
 void print_wind(struct Wind* wind) {
-    const char* direction[4] = {"NORTH","EAST","SOUTH","WEST"};
+    const char* direction[4] = {"NORTH","EAST","SOUTH","WEST"}; //Char array til at match med direction
     printf("Direction is: %s\n",direction[wind->direction]);
     printf("Wind speed is: %lf\n",wind->speed);
 }
