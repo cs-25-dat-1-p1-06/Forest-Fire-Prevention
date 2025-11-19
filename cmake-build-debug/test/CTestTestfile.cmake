e2ca5d547539d6ee12145df0382f12e770130c99
FedKat <nicklasmoestrup@gmail.com>
2025-11-19 12:40:42 +0100

    Added the user_mouse_input and console_setup functions

---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/nickl/CLionProjects/Forest-Fire-Prevention/test
+# Build directory: C:/Users/nickl/CLionProjects/Forest-Fire-Prevention/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
