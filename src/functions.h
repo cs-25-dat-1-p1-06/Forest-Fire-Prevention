fdb4552680fabbbdd4db3c435bf3b4a5fdd8aa0c
okjar24 <gc82gq@student.aau.dk>
2025-11-19 12:20:17 +0100

    Tilføjet: user_drop_water og user_dead_zone funktioner

--- src/functions.h
+++ src/functions.h
@@ -4,7 +4,7 @@
 //
 #ifndef FOREST_FIRE_PREVENTION_FUNCTIONS_H
 #define FOREST_FIRE_PREVENTION_FUNCTIONS_H
-typedef enum {empty, fresh, burning, burnt} status_e;
+typedef enum {empty, fresh, burning, burnt, wet} status_e;
 typedef struct {
     status_e status;
     int humidity;
@@ -12,12 +12,14 @@ typedef struct {
 } tree_t;
 void make_rnd_forest(tree_t* forest, double density, int size);
 void print_forest(tree_t* forest, int height, int width);
-tree_t get_tree(int x, int y, int width, tree_t* forest);
+tree_t* get_tree(int x, int y, int width, tree_t* forest);
 void scan_settings(int* width, int* height, double* density);
 
 int chance(int procent,bool forest_thinning,bool is_wet);
 
-tree_t* check_surrounding(tree_t* forest, tree_t* surrounding, int x, int y,int width, int height);
+void check_surrounding(tree_t* forest, int x, int y,int width, int height);
+void user_drop_water(tree_t* forest, int x, int y, int width);
+void user_dead_zone(tree_t* forest, int x, int y, int width, int size_of_dead_zone);
 
 
 #endif //FOREST_FIRE_PREVENTION_FUNCTIONS_H
--- src/main.c
+++ src/main.c
@@ -8,12 +8,8 @@
 #include <time.h>
 #include <stdbool.h>
 
-
-
-
 int main(void) {
     srand(time(NULL));
-    tree_t* surrounding = malloc(9*sizeof(tree_t));
     int width, height;
     double density;
     scan_settings(&width, &height, &density);
@@ -24,14 +20,18 @@ int main(void) {
     int y = 1;
 
     //Vi printer træets koordinater ud fra at øverste venstre hjørne, er (0,0) og printer dets værdi
-    printf("Tree at (%d,-%d) has the status %d\n", x, y, get_tree(x,y,width,forest).status );
-    printf("%d"\n, forest[width*y+x].status);
+    printf("Tree at (%d,-%d) has the status %d\n", x, y, get_tree(x,y,width,forest)->status );
+    printf("%d\n", forest[width*y+x].status);
     chance(100,1,0);
     printf("The statuses of the trees surrounding (x,y) are the following\n");
-
-    surrounding = check_surrounding(forest, surrounding, x,y,width,height);
+    check_surrounding(forest, x,y,width,height);
+    printf("You have dropped water on the area surrounding (x,y)\n");
+    user_drop_water(forest, x, y, width);
+    printf("You have made a dead zone of 3 trees in each direction around (x+2,y+2)\n");
+    user_dead_zone(forest,x+2,y+2,width,3);
+    print_forest(forest, height, width);
     free(forest);
-    free(surrounding);
+
 
     return 0;
 }
