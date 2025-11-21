88d271cf50ef8bab3efaf8b0ddfa43249be278da
Hartv1g <hartvigkristoffer@gmail.com>
2025-11-21 09:32:05 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hartv/Desktop/Programmering/Projekter/Forest-Fire-Prevention/test
-# Build directory: C:/Users/hartv/Desktop/Programmering/Projekter/Forest-Fire-Prevention/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
