70189155f2bcea0221e63302dff323b00ec57c4d
okjar24 <gc82gq@student.aau.dk>
2025-11-26 14:35:53 +0100

    Working Prototype with comments for fire-sim

--- src/console.c
+++ src/console.c
@@ -1,15 +1,15 @@
 #include "console.h"
+HANDLE hConsole;
+
 
 void color_change(unsigned short color)
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
     SetConsoleTextAttribute(hConsole, color);
 }
 
 void console_setup()
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     //hides the cursor
     CONSOLE_CURSOR_INFO cursorInfo;
@@ -20,4 +20,10 @@ void console_setup()
     //maximizes window
     HWND consoleWindow = GetConsoleWindow(); // This gets the value Windows uses to identify your output window
     ShowWindow(consoleWindow, SW_MAXIMIZE); // this mimics clicking on its maximize button
+}
+
+void reset_color() {
+    color_change(0);
+    color_change(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
 }
--- src/console.h
+++ src/console.h
@@ -1,7 +1,11 @@
 #ifndef FOREST_FIRE_PREVENTION_CONSOLE_H
 #define FOREST_FIRE_PREVENTION_CONSOLE_H
 #include <windows.h>
+extern HANDLE hConsole;
+
 void color_change(unsigned short color);
 void console_setup();
+void reset_color();
+
 
 #endif //FOREST_FIRE_PREVENTION_CONSOLE_H
